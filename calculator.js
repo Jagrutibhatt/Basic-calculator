62dbca96939b0b45591a195c48e14c1d14a6a0f8
Jagrutibhatt <109428301+Jagrutibhatt@users.noreply.github.com>
2022-07-16 21:11:36 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,44 @@
+
+
+function addition(x,y){
+
+var result = x+y;
+return result;
+
+}
+
+var output= addition (2,3);
+console.log(output);
+ // *************************************
+function subtraction (a,b){
+
+var result = a-b;
+return result;
+
+}
+var output = subtraction (30,25)
+console.log(output)
+
+//*********************** */
+
+function multiply(q,w) {
+
+    var result = q*w;
+    return result;
+
+}
+
+var output = multiply (5,5)
+console.log(output)
+
+//************************ */
+
+function divide (r,t) {
+
+    var result = r/t;
+    return result;
+
+}
+
+var output = divide (20,10)
+console.log(output)
